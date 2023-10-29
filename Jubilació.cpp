a03fa18f35977281f33e2a0644ec710f04171bc0
dafloresdp <149301894+dafloresdp@users.noreply.github.com>
2023-10-29 12:32:10 +0100

    Add files via upload

---
+++ Jubilació.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main () {
+string Nom;
+int edat, resultat;
+cout <<"Com et dius?"<<endl;
+cin >>Nom;
+cout <<"Quants anys tens?"<<endl;
+cin >>edat;
+resultat = 65 - edat ;
+cout <<"Hola "<<Nom<<", et falten "<<resultat<<" anys per jubilarte!"<<endl;
+cout <<"L'edat per poder jubilar-se son 65 anys"<<endl;
+
+
+
+}
