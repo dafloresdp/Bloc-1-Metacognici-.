baad11855247fe948ef03628c589fd810a8f15c2
dafloresdp <149301894+dafloresdp@users.noreply.github.com>
2023-10-29 12:36:40 +0100

    Add files via upload

---
+++ Calcul area i preimetre.cpp
@@ -0,0 +1,14 @@
+#include <iostream>;
+using namespace std;
+int main () {
+int costat, resultat1, resultat2;
+cout <<"Introdueix la mida del costat d'un quadrat: "<<endl;
+cin>>costat;
+resultat1 = costat * 4;
+resultat2= costat * costat;
+
+cout <<"Perimetre del quadrat: "<<resultat1<<endl;
+cout <<"Area del quadrat: "<<resultat2<<endl;
+
+
+}
