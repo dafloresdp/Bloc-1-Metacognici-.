b2c8a38ed62287d560e0ce1b50222ad7e648afe4
dafloresdp <149301894+dafloresdp@users.noreply.github.com>
2023-10-29 12:04:47 +0100

    Add files via upload

---
+++ Divisió amb residu.cpp
@@ -0,0 +1,15 @@
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+int divisio, residu;
+int a, b;
+cout <<"introdueix la quantitat indicada: " ;
+cin >> a >> b;
+divisio = ( a/b);
+residu = (a % b);
+cout <<"resultat ha estat"<< a << ":" <<b<< "=" << divisio << " i residu " << residu << endl;
+
+}
