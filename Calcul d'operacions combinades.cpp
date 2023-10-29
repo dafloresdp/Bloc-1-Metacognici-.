b1fb9d2b4696c1c0f93667453ed47c88b072e848
dafloresdp <149301894+dafloresdp@users.noreply.github.com>
2023-10-29 12:12:45 +0100

    Add files via upload

---
+++ Calcul d'operacions combinades.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main () {
+
+cout<<"Aquest ha sigut el primer resultat: "<<10*2-4<<endl;
+cout<<"Aquest ha sigut el segon resultat: "<<10* (2-4)<<endl;
+
+
+}
