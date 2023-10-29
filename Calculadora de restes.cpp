806e8b2f5d1db68a96668bf89ed83a8859da8fc4
dafloresdp <149301894+dafloresdp@users.noreply.github.com>
2023-10-29 12:24:36 +0100

    Add files via upload

---
+++ Calculadora de restes.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main () {
+int nombre1;
+int nombre2;
+int resultat;
+cout<<"Introdueix el teu primer nombre: "<<endl;
+cin>>nombre1;
+cout <<"Introdueix el teu segon nombre: "<<endl;
+cin>>nombre2;
+resultat=nombre1 - nombre2;
+cout<<"El resultat" <<nombre1 << "-" <<nombre2 << "es: " <<resultat <<endl;
+}
